8e144e7182f2b2544eada38eb16b5d979665e914
chronosacaria <chronos.droid@gmail.com>
2024-04-20 18:18:14 -0400

    Added Copyright Notice to Headers

--- src/main/java/chronosacaria/mcdw/enums/IMcdwWeaponID.java
+++ src/main/java/chronosacaria/mcdw/enums/IMcdwWeaponID.java
@@ -1,3 +1,9 @@
+/*
+Timefall Development License 1.2
+Copyright (c) 2020-2024. Chronosacaria, Kluzzio, Timefall Development. All Rights Reserved.
+
+This software's content is licensed under the Timefall Development License 1.2. You can find this license information here: https://github.com/Timefall-Development/Timefall-Development-Licence/blob/main/TimefallDevelopmentLicense1.2.txt
+*/
 package chronosacaria.mcdw.enums;
 
 import net.minecraft.item.Item;
--- src/main/java/chronosacaria/mcdw/items/BeeStingerItem.java
+++
@@ -1,9 +0,0 @@
-//package chronosacaria.mcdw.items;
-//
-//import net.minecraft.item.Item;
-//
-//public class BeeStingerItem extends Item {
-//    public BeeStingerItem(Settings settings){
-//        super(settings);
-//    }
-//}
